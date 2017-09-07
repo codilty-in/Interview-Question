6cf402ad994c19a04877393ac211039ed9e8e072
Nidhi Desai <nidhi.desai990@gmail.com>
2017-09-07 19:50:40 -0400

    Codility Interview Question

--- CountWords.java
+++ CountWords.java
@@ -6,7 +6,7 @@ public class CountWords {
     public int solution(String S){
         int pre=0;
         int post=0;
-        String[] sentences=S.split("(?<=[,.?])|(?=[,.?])");
+        String[] sentences=S.split("(?<=[,.?!])");
         for(int i=0;i<sentences.length;i++){
             String[] words=sentences[i].split(".\\s+");
             post=words.length;
@@ -28,7 +28,7 @@ public class CountWords {
 
     public static void main(String[] args) {
         CountWords c=new CountWords();
-     int op=c.solution("Give us a Try. I hope all is well? Don't worry");
+     int op=c.solution("Give us a Try! I hope all is well! Don't worry? My name is Nidhi Desai and I am Graduate Student");
         System.out.println(op);
     }
 }
